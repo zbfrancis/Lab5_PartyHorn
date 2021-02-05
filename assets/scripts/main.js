13001a036ff3433bdd488aa10195615257a4d1b9
zbfrancis <zbfranci@ucsd.edu>
2021-02-04 21:14:10 -0800

    changeVol

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,20 @@
 // main.js
-function playSound() {
-    document.getElementById("horn-sound").play();
+
+let sound = document.getElementById("horn-sound");
+let vol_number = document.getElementById("volume-number");
+
+function attachListeners() {
+    vol_number.addEventListener("change", changeVol);
+    vol_number.addEventListener("input", changeVol);
+}
+
+function changeVol() {
+    sound.volume = (vol_number.valueAsNumber)/100;
 }
 
 document.getElementById("honk-btn").addEventListener("click", playSound);
+
+function playSound() {
+    sound.getElementById("horn-sound").play();
+}
+
